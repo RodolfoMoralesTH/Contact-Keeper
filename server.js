8be79e6a7f228446e5e3408b0507c02364db652a
AgnesMoralesTH <agnesmoralesth@gmail.com>
2021-07-05 18:55:46 -0700

    added express validator

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 //connect Database
 connectDB();
 
+//Init Middleware
+app.use(express.json({extended: false}))
+
 app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeeper API.' }));
 
 //Define Routes
