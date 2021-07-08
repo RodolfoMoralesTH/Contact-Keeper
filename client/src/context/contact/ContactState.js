dcd7641491198d24eae6d68c0d11a2a6d293773c
AgnesMoralesTH <agnesmoralesth@gmail.com>
2021-07-07 19:26:35 -0700

    added delete and put capabilities for contact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -32,7 +32,8 @@ const ContactState = props => {
                 phone: '333-333-3333',
                 type: 'professional'
             }
-        ]
+        ],
+        current: null
     }
 
     const [state, dispatch] = useReducer(contactReducer, initialState)
@@ -47,10 +48,18 @@ const ContactState = props => {
         
         dispatch({type: DELETE_CONTACT, payload: id });
     }
+    
     //Set Current Contact
+    const setCurrent = contact =>{
+        
+        dispatch({type: SET_CURRENT, payload: contact });
+    }
 
     //Clear Current Contact
-
+    const clearCurrent = () =>{
+        
+        dispatch({type: CLEAR_CURRENT });
+    }
     //Update Contact
 
     //Filter Contacts
@@ -60,8 +69,11 @@ const ContactState = props => {
     return (
         <ContactContext.Provider value={{
             contacts: state.contacts,
+            current: state.current,
             addContact,
-            deleteContact
+            deleteContact,
+            setCurrent,
+            clearCurrent
         }}>{ props.children }</ContactContext.Provider>
     )
 }
