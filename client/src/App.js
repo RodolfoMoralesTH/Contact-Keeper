746f54a0339e2d6b0527c2dde68c9e94d6666197
AgnesMoralesTH <agnesmoralesth@gmail.com>
2021-07-08 05:56:45 -0700

    added an login and register route, user state for updating form data, added links to navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,9 @@ import React, {Fragment} from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Home from './components/pages/Home';
-import About from './components/pages/About'
+import About from './components/pages/About';
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
 import ContactState from './context/contact/ContactState'
 import AuthState from './context/auth/AuthState';
 
@@ -18,6 +20,8 @@ const App =()=> {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
         </Switch>
       </div>
     </Fragment>
---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react'
+
+const Login = () => {
+    const [user, setUser] = useState({
+       email: '',
+        password: ''
+        
+    })
+
+    const { email,password } = user;
+
+    const onChange = e => setUser({...user, [e.target.name]: e.target.value})
+
+    const onSubmit = e => {
+        e.preventDefault();
+        console.log('Login');
+    }
+    
+    return (
+        <div className="form-container">
+            <h1>
+                Account <span className="text-primary">
+                    Login
+                </span>
+
+                <form onSubmit={onSubmit}>
+                    
+                    <div className="form-group">
+                        <label htmlFor="email">Email Address</label>
+                        <input type="email" name="email" value={email}
+                        onChange={onChange} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" value={password}
+                        onChange={onChange} />
+                    </div>
+                    
+                    <input type="submit" value="Login" className="btn btn-primary btn-block" />
+                </form>
+            </h1>
+        </div>
+    )
+}
+export default Login
